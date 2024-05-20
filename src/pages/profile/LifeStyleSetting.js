bfef642ab3a0b504140cb09588f7dfa6af937cfd
k7sourabh <k7sourabh@gmail.com>
2024-05-20 19:18:23 +0530

    LifestyleSetting

--- src/pages/profile/LifeStyleSetting.js
+++ src/pages/profile/LifeStyleSetting.js
@@ -13,21 +13,15 @@ import {
 } from '@ionic/react';
 import React, { useEffect, useState } from 'react';
 import { getApiData, postApiData } from '../../utils/Utils';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useHistory } from 'react-router';
 
 const LifeStyleSetting = () => {
-
     const [lifestyleHealth, setLifestyleHealth] = useState([]);
     const [lifestyleActivity, setLifestyleActivity] = useState([]);
-    const history=useHistory()
+    const [selectedHealth, setSelectedHealth] = useState([]);
+    const [selectedActivity, setSelectedActivity] = useState([]);
+    const history = useHistory();
     const [present] = useIonToast();
-
-    const initialValues = {
-        health: [],
-        activity: []
-    };
-
 
     const fetchLifeStyleData = async () => {
         try {
@@ -43,30 +37,41 @@ const LifeStyleSetting = () => {
         fetchLifeStyleData();
     }, []);
 
-   
+    const handleHealthChange = (event) => {
+        const { value, checked } = event.target;
+        setSelectedHealth((prevSelected) =>
+            checked ? [...prevSelected, value] : prevSelected.filter((item) => item !== value)
+        );
+    };
 
-    const handleSubmit = async (values, { resetForm }) => {
-        // console.log("values",values)
+    const handleActivityChange = (event) => {
+        const { value, checked } = event.target;
+        setSelectedActivity((prevSelected) =>
+            checked ? [...prevSelected, value] : prevSelected.filter((item) => item !== value)
+        );
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
         try {
             const formData = new FormData();
-            formData.append('health', JSON.stringify(values.health));
-            formData.append('activity', JSON.stringify(values.activity));
+            formData.append('health', JSON.stringify(selectedHealth));
+            formData.append('activity', JSON.stringify(selectedActivity));
 
             const response = await postApiData('lifestyle-setting-post', formData);
-            // console.log("response", response);
-            if(response?.data?.status===200){
+            if (response?.data?.status === 200) {
                 presentToast("Top", response?.data?.lifestyle_setting);
-                setTimeout(()=>{
-                    history.push("/profile")
-                },3000)
-               
+                setSelectedHealth([]);  // Reset selected health
+                setSelectedActivity([]);  // Reset selected activity
+                setTimeout(() => {
+                    history.push("/profile");
+                }, 3000);
             }
-            resetForm();
-
         } catch (err) {
             console.log(err);
         }
     };
+
     const presentToast = (position, message) => {
         present({
             message: message,
@@ -77,7 +82,6 @@ const LifeStyleSetting = () => {
 
     return (
         <IonPage>
-            {/* <Header /> */}
             <IonContent>
                 <IonHeader className="TitleHead bottom-shadow">
                     <IonButton className="backBtn" fill="clear" routerLink="/profile">
@@ -85,75 +89,61 @@ const LifeStyleSetting = () => {
                     </IonButton>
                     <IonTitle color="dark">Life Style Setting</IonTitle>
                 </IonHeader>
-                <Formik
-                    initialValues={initialValues}
-                    onSubmit={handleSubmit}
-                >
-                    {({ isSubmitting }) => (
-                        <Form>
-                            <IonGrid className='ion-padding-horizontal'>
-                                <h3>Health</h3>
-                                <IonRow className='d-flex ion-justify-content-center'>
-                                    {lifestyleHealth?.map((item, i) => (
-                                        <IonCol size='6' key={i}>
-                                            <div lines='none' className='LifeStyle'>
-                                                <Field
-                                                    type="checkbox"
-                                                    name="health"
-                                                    value={item.setting_name}
-                                                    id={`CheckHealth${i}`}
-                                                />
-                                                <label htmlFor={`CheckHealth${i}`}>
-                                                    <img src={item.images} alt="Health Setting" className="ProfileImg" />
-                                                    {/* <IonText>{item.setting_name}</IonText> */}
-                                                </label>
-                                            </div>
-                                          
-                                        </IonCol>
-                                    ))}
-                                </IonRow>
-                                <IonRow>
-                                    <IonCol>
-                                        <ErrorMessage name="health" component="div" style={{ color: 'red', textAlign: 'center' }} />
-                                    </IonCol>
-                                </IonRow>
-                            </IonGrid>
-                            <IonGrid className='ion-padding-horizontal'>
-                                <h3>Activity</h3>
-                                <IonRow className='d-flex ion-justify-content-center'>
-                                    {lifestyleActivity?.map((item, i) => (
-                                        <IonCol size='6' key={i}>
-                                            <div lines='none' className='LifeStyle'>
-                                                <Field
-                                                    type="checkbox"
-                                                    name="activity"
-                                                    value={item.setting_name}
-                                                    id={`CheckActivity${i}`}
-                                                />
-                                                <label htmlFor={`CheckActivity${i}`}>
-                                                    <img src={item.images} alt="" className="ProfileImg" />
-                                                    <IonText className='img-text'>{item.setting_name}</IonText>
-                                                </label>
-                                            </div>
-                                        </IonCol>
-                                    ))}
-                                </IonRow>
-                                <IonRow>
-                                    <IonCol>
-                                        <ErrorMessage name="activity" component="div" style={{ color: 'red', textAlign: 'center' }} />
-                                    </IonCol>
-                                </IonRow>
-                                <IonRow>
-                                    <IonCol>
-                                        <IonButton type="submit"disabled={isSubmitting} >Save</IonButton>
-                                    </IonCol>
-                                </IonRow>
-                            </IonGrid>
-                        </Form>
-                    )}
-                </Formik>
+                <form onSubmit={handleSubmit}>
+                    <IonGrid className='ion-padding-horizontal'>
+                        <h3>Health</h3>
+                        <IonRow className='d-flex ion-justify-content-center'>
+                            {lifestyleHealth.map((item, i) => (
+                                <IonCol size='6' key={i}>
+                                    <div lines='none' className='LifeStyle'>
+                                        <input
+                                            type="checkbox"
+                                            name="health"
+                                            value={item.setting_name}
+                                            id={`CheckHealth${i}`}
+                                            onChange={handleHealthChange}
+                                            checked={selectedHealth.includes(item.setting_name)}
+                                        />
+                                        <label htmlFor={`CheckHealth${i}`}>
+                                            <img src={item.images} alt="Health Setting" className="ProfileImg" />
+                                        </label>
+                                    </div>
+                                </IonCol>
+                            ))}
+                        </IonRow>
+                    </IonGrid>
+                    <IonGrid className='ion-padding-horizontal'>
+                        <h3>Activity</h3>
+                        <IonRow className='d-flex ion-justify-content-center'>
+                            {lifestyleActivity.map((item, i) => (
+                                <IonCol size='6' key={i}>
+                                    <div lines='none' className='LifeStyle'>
+                                        <input
+                                            type="checkbox"
+                                            name="activity"
+                                            value={item.setting_name}
+                                            id={`CheckActivity${i}`}
+                                            onChange={handleActivityChange}
+                                            checked={selectedActivity.includes(item.setting_name)}
+                                        />
+                                        <label htmlFor={`CheckActivity${i}`}>
+                                            <img src={item.images} alt="" className="ProfileImg" />
+                                            <IonText className='img-text'>{item.setting_name}</IonText>
+                                        </label>
+                                    </div>
+                                </IonCol>
+                            ))}
+                        </IonRow>
+                        <IonRow>
+                            <IonCol>
+                                <IonButton type="submit">Save</IonButton>
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                </form>
             </IonContent>
         </IonPage>
     );
 };
-export default LifeStyleSetting
+
+export default LifeStyleSetting;
