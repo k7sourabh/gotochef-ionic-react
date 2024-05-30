cb5e3dc75e74f2ee9ace9899401ee8dc1c187c84
SOURABH <k7sourabh@gmail.com>
2024-05-30 19:08:00 +0530

    api integrated on dashboard

--- src/pages/profile/Badges.js
+++ src/pages/profile/Badges.js
@@ -1,131 +1,110 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonText, IonTitle } from '@ionic/react'
-import { lockClosedOutline } from 'ionicons/icons'
-import React from 'react'
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonRow,
+  IonText,
+  IonTitle,
+} from "@ionic/react";
+import { lockClosedOutline } from "ionicons/icons";
+import React, { useEffect, useState } from "react";
+import { getApiData } from "../../utils/Utils";
 
 const Badges = () => {
-  return (
-  <IonPage>
-    <IonContent>
-    <IonHeader className="TitleHead bottom-shadow">
-        <IonButton className="backBtn" fill="clear" routerLink="/dashboard">
-            <i class="material-icons dark">west</i>
-        </IonButton>
-        <IonTitle color="dark">Badges</IonTitle>
-    </IonHeader>
-    <IonGrid class="ion-padding-horizontal">
-    <IonRow className='ion-padding-vertical'>
-        <IonCol>
-            <IonTitle className='ion-no-padding'>
-                BADGE COLLECTION
-            </IonTitle>
-        </IonCol>
-    </IonRow>
-    <IonRow className='ion-padding-vertical'>
-        <IonCol size='6'>
-            <div className="review-award">
-                <img src="./assets/img/reviewRecipe.png" alt="award" />
-                <h4 className='ion-no-margin'>1 Review</h4>
-                <div className="line"></div>
-                <IonText>Review</IonText>
-            </div>
-        </IonCol>
-        <IonCol size='6'>
-            <div className="review-award">
-                <img src="./assets/img/reviewRecipe.png" alt="award" />
-                <h4 className='ion-no-margin'>1 Review</h4>
-                <div className="line"></div>
-                <IonText>Review</IonText>
-            </div>
-        </IonCol>
-    </IonRow>
-    <IonRow className='ion-padding-vertical'>
-        <IonCol size='6'>
-            <div className="review-award">
-                <IonIcon
-                    color="primary"
-                    size="small"
-                    className='review-lock'
-                    icon={lockClosedOutline} />
-                <img src="./assets/img/helpful_review.png" alt="award" className='review-help' />
-                <h4 className='ion-no-margin'>1 Review</h4>
-                <div className="line"></div>
-                <IonText>Review</IonText>
-            </div>
-        </IonCol>
-        <IonCol size='6'>
-            <div className="review-award">
-                <IonIcon
-                    color="primary"
-                    size="small"
-                    className='review-lock'
-                    icon={lockClosedOutline} />
-                <img src="./assets/img/helpful_review.png" alt="award" className='review-help' />
-                <h4 className='ion-no-margin'>1 Review</h4>
-                <div className="line"></div>
-                <IonText>Review</IonText>
-            </div>
-        </IonCol>
-    </IonRow>
-    <IonRow className='ion-padding-vertical'>
-        <IonCol size='6'>
-            <div className="review-award">
-                <IonIcon
-                    color="primary"
-                    size="small"
-                    className='review-lock'
-                    icon={lockClosedOutline} />
-                <img src="./assets/img/helpful_review.png" alt="award" className='review-help' />
-                <h4 className='ion-no-margin'>1 Review</h4>
-                <div className="line"></div>
-                <IonText>Review</IonText>
-            </div>
-        </IonCol>
-        <IonCol size='6'>
-            <div className="review-award">
-                <IonIcon
-                    color="primary"
-                    size="small"
-                    className='review-lock'
-                    icon={lockClosedOutline} />
-                <img src="./assets/img/helpful_review.png" alt="award" className='review-help' />
-                <h4 className='ion-no-margin'>1 Review</h4>
-                <div className="line"></div>
-                <IonText>Review</IonText>
-            </div>
-        </IonCol>
-    </IonRow>
-    <IonRow className='ion-padding-vertical'>
-        <IonCol size='6'>
-            <div className="review-award">
-                <IonIcon
-                    color="primary"
-                    size="small"
-                    className='review-lock'
-                    icon={lockClosedOutline} />
-                <img src="./assets/img/helpful_review.png" alt="award" className='review-help' />
-                <h4 className='ion-no-margin'>1 Review</h4>
-                <div className="line"></div>
-                <IonText>Review</IonText>
-            </div>
-        </IonCol>
-        <IonCol size='6'>
-            <div className="review-award">
-                <IonIcon
-                    color="primary"
-                    size="small"
-                    className='review-lock'
-                    icon={lockClosedOutline} />
-                <img src="./assets/img/helpful_review.png" alt="award" className='review-help' />
-                <h4 className='ion-no-margin'>1 Review</h4>
-                <div className="line"></div>
-                <IonText>Review</IonText>
-            </div>
-        </IonCol>
-    </IonRow>
-    </IonGrid>
-    </IonContent>
-  </IonPage>
-  )
-}
+  const [badgesData, setBadgesData] = useState({});
 
-export default Badges
+  const recipeDetails = async () => {
+    try {
+      const response = await getApiData(`/badges-collection`);
+      console.log(response?.data);
+      setBadgesData(response?.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    recipeDetails();
+  }, []);
+  console.log(badgesData?.my_badges);
+  return (
+    <IonPage>
+      <IonContent>
+        <IonHeader className="TitleHead bottom-shadow">
+          <IonButton className="backBtn" fill="clear" routerLink="/dashboard">
+            <i class="material-icons dark">west</i>
+          </IonButton>
+          <IonTitle color="dark">Badges</IonTitle>
+        </IonHeader>
+        <IonGrid class="ion-padding-horizontal">
+          <IonRow className="ion-padding-vertical">
+            <IonCol>
+              <IonTitle className="ion-no-padding">BADGE COLLECTION</IonTitle>
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-padding-vertical">
+            {badgesData &&
+              badgesData?.my_badges?.map((data, i) => (
+                <IonCol size="6">
+                  <div className="review-award">
+                    <img
+                      src={
+                        !data.images
+                          ? "/assets/img/img-placeholder.jpg"
+                          : data.images
+                      }
+                      onError={(e) => {
+                        e.target.onerror = null;
+                        e.target.src = "/assets/img/img-placeholder.jpg";
+                      }}
+                      alt="award"
+                    />
+                    <h4 className="ion-no-margin">{data.num_review} Review</h4>
+                    <div className="line"></div>
+                    <IonText>Review</IonText>
+                  </div>
+                </IonCol>
+              ))}
+          </IonRow>
+
+          <IonRow className="ion-padding-vertical">
+            {badgesData &&
+              badgesData?.up_coming_badges?.map((data, i) => (
+                <IonCol size="6">
+                  <div className="review-award">
+                    <IonIcon
+                      color="primary"
+                      size="small"
+                      className="review-lock"
+                      icon={lockClosedOutline}
+                    />
+                    <img
+                      src={
+                        !data.images
+                          ? "/assets/img/img-placeholder.jpg"
+                          : data.images
+                      }
+                      onError={(e) => {
+                        e.target.onerror = null;
+                        e.target.src = "/assets/img/img-placeholder.jpg";
+                      }}
+                      alt="award"
+                    />
+                    <h4 className="ion-no-margin">{data.num_review} Review</h4>
+                    <div className="line"></div>
+                    <IonText>Review</IonText>
+                  </div>
+                </IonCol>
+              ))}
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Badges;
